ae3e832247830a2436d66ad4b552761c10398290
Shingo Hiraya <shingohiraya@gmail.com>
2017-07-16 21:06:31 +0900

    Update MarkerAnnotationViewViewController.swift

--- iOS11MapKitSample/MarkerAnnotationViewViewController.swift
+++ iOS11MapKitSample/MarkerAnnotationViewViewController.swift
@@ -23,23 +23,63 @@ extension MarkerAnnotationViewViewController {
     func prepare() {
         mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.65858, longitude: 139.745433),
                                             span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
-        mapView.delegate = self
-        
-        mapView.register(MKMarkerAnnotationView.self,
+        mapView.register(LocationView.self,
                          forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        mapView.mapType = .mutedStandard
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 35.65858, longitude: 139.745433)
+        let annotation = Location()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 35.65858,
+                                                       longitude: 139.745433)
         annotation.title = "東京タワー"
         annotation.subtitle = "Tokyo Tower"
+        annotation.type = .famousPlace
         mapView.addAnnotation(annotation)
+        
+        let annotation2 = Location()
+        annotation2.coordinate = CLLocationCoordinate2D(latitude: 35.655353,
+                                                        longitude: 139.743462)
+        annotation2.title = "ファミリーマート"
+        annotation2.subtitle = "東麻布一丁目店"
+        annotation2.type = .shop
+        mapView.addAnnotation(annotation2)
+        
+        let annotation3 = Location()
+        annotation3.coordinate = CLLocationCoordinate2D(latitude: 35.654682,
+                                                        longitude: 139.749910)
+        annotation3.title = "芝公園駅"
+        annotation3.subtitle = "都営大江戸線"
+        annotation3.type = .other
+        mapView.addAnnotation(annotation3)
     }
 }
 
-extension MarkerAnnotationViewViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var view = mapView.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier) as! MKMarkerAnnotationView
-        view.glyphText = "A+"
-        return view
+fileprivate class Location: MKPointAnnotation {
+    enum LocationType: Int {
+        case famousPlace
+        case shop
+        case other
+    }
+    
+    var type: LocationType = .other
+}
+
+fileprivate class LocationView: MKMarkerAnnotationView {
+    override var annotation: MKAnnotation? {
+        willSet {
+            if let location = newValue as? Location {
+                switch location.type {
+                case .famousPlace:
+                    glyphText = "観光"
+                    markerTintColor = UIColor(named: "Blue")
+                case .shop:
+                    // Icon by https://iconmonstr.com/
+                    glyphImage = UIImage(named: "sho-cart")
+                    
+                    markerTintColor = UIColor(named: "Green")
+                case .other:
+                    break
+                }
+            }
+        }
     }
 }
